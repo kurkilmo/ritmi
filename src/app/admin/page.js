3dd96ed10fc15e46ab3d46d13d41cee3464bf59c
kurkilmo <ilmo.kurki00@gmail.com>
2025-01-24 22:19:45 +0200

    Move reorder to /admin

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -1,27 +1,105 @@
-import { getAllSongs } from "@/services/songservice"
-import Link from "next/link"
-import { redirect } from "next/navigation"
+'use client'
 
-export default async function Admin() {
-    const newSong = async () => {
-        'use server'
-        redirect('/admin/new')
+import { useState, useEffect } from 'react'
+import { useRouter } from 'next/navigation'
+import Link from 'next/link'
+import { Reorder } from "framer-motion"
+
+
+export default function Admin() {
+    const [songs, setSongs] = useState([])
+    const [nSongs, setNSongs] = useState("2")
+    const [nSongsInt, setNSongsInt] = useState(2)
+    const defaultFieldColor = "white"
+    const [nFieldColor, setNFieldColor] = useState(defaultFieldColor)
+
+    const router = useRouter()
+
+    useEffect(() => {
+        fetch('/api/songs')
+        .then((res) => res.json())
+        .then((data) => {
+            setSongs(data)
+            let i = data.length - 1
+            while (isNaN(data[i--].number)) { }
+            setNSongs("" + data.length - i - 2)
+            setNSongsInt(data.length - i - 2)
+        })
+    }, [])
+
+    const numberSong = (song) => {
+        const maxIndex = songs.length - 1
+        const songIndex = songs.indexOf(song)
+
+        for (let i = 0; i < nSongsInt; i++) {
+            if (songIndex === maxIndex - i) return "n" + (i !== 0 ? "-" + i : "")
+        }
+        return (songIndex + 1)
     }
 
-    const songs = await getAllSongs()
+    const SongView = ({ song }) => {
+        return (
+            <div className="reorderEntry">
+                <Link href={`/admin/edit/${song.url}`}>{song.title}</Link>
+                <h1>{numberSong(song)}</h1>
+            </div>
+        )
+    }
 
-    return (
-        <div className="admin">
-            <button onClick={newSong}>Uusi laulu</button>
-            <div className="songEditList">
-                <p>Muokkaa lauluja:</p>
-                <ul>
-                    {songs.map((song, ind) => (
-                        <li key={ind}>
-                            <Link href={`/admin/edit/${song.url}`}>{song.title}</Link>
-                        </li>
-                    ))}
-                </ul>
+    const validateN = (event) => {
+        event.preventDefault()
+        setNSongs(event.target.value)
+        const int = parseInt(event.target.value)
+
+        if (isNaN(int)) {
+            setNFieldColor("red")
+        } else {
+            setNFieldColor(defaultFieldColor)
+            setNSongsInt(int)
+        }
+    }
+
+    const saveOrdering = () => {
+        fetch(`/api/songs`, {
+            method: "PUT",
+            body: JSON.stringify(songs.map(song => (
+                { ...song, number:numberSong(song) }
+            )))
+        })
+    }
+
+    const SaveNumberingButton = () => {
+        return (
+            <button onClick={saveOrdering} >tallenna järjestys</button>
+        )
+    }
+
+    return(
+        <div className="reorder">
+            <div className="reorderHeader">
+                <button onClick={() => router.push('/admin/new')}>Uusi</button>
+                <SaveNumberingButton />
+            </div>
+            <Reorder.Group values={songs} onReorder={setSongs}>
+                {songs.map(song => (
+                    <Reorder.Item key={song.number} value={song}>
+                        <SongView song={song} />
+                    </Reorder.Item>
+                ))}
+            </Reorder.Group>
+            <div className="reorderFooter">
+                <SaveNumberingButton />
+                
+                n: 
+                <input
+                    type="number"
+                    value={nSongs}
+                    onChange={validateN}
+                    style={{
+                        background:nFieldColor,
+                        width:"4em"
+                    }}
+                />
             </div>
         </div>
     )
